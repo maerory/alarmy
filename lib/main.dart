e6835e5bc948e889aff2306f96bdf87aa5fd6b7c
Joey Yoo <joowooyoo@gmail.com>
2021-04-21 23:29:34 +0900

    alarm notif.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:alarmy/enums.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'views/homepage.dart';
 import 'package:provider/provider.dart';
 import 'package:alarmy/models/menu_info.dart';
 
-void main() {
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('app_icon');
+  final IOSInitializationSettings initializationSettingsIOS =
+      IOSInitializationSettings(
+          onDidReceiveLocalNotification:
+              (int id, String title, String body, String payload) async {});
+  final MacOSInitializationSettings initializationSettingsMacOS =
+      MacOSInitializationSettings();
+  final InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsIOS,
+      macOS: initializationSettingsMacOS);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification: (String payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: ' + payload);
+    }
+  });
+
   runApp(MyApp());
 }
 
--- lib/views/alarm_page.dart
+++ lib/views/alarm_page.dart
@@ -1,7 +1,11 @@
+import 'package:alarmy/main.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:alarmy/constants/theme_data.dart';
 import 'package:alarmy/data.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 class AlarmPage extends StatefulWidget {
   @override
@@ -109,7 +113,9 @@ class _AlarmPageState extends State<AlarmPage> {
                   padding:
                       const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
                   child: TextButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        scheduleAlarm();
+                      },
                       child: Column(children: <Widget>[
                         Image.asset(
                           'assets/add_alarm.png',
@@ -127,5 +133,35 @@ class _AlarmPageState extends State<AlarmPage> {
         ))
       ]),
     );
+  }
+
+  void scheduleAlarm() async {
+    tz.initializeTimeZones();
+    var scheduledNotificationDateTime =
+        tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5));
+
+    const androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'alarm_notif',
+      'alarm_notif',
+      'Channel for Alarm notification',
+      icon: "app_icon",
+      sound: RawResourceAndroidNotificationSound('siren'),
+      largeIcon: DrawableResourceAndroidBitmap('app_icon'),
+    );
+
+    const iOSPlatformChannelSpecifics = IOSNotificationDetails(
+        sound: 'siren.wav',
+        presentAlert: true,
+        presentBadge: true,
+        presentSound: true);
+
+    const NotificationDetails platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+        iOS: iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.zonedSchedule(0, "Office",
+        "Good Morning", scheduledNotificationDateTime, platformChannelSpecifics,
+        androidAllowWhileIdle: true,
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime);
   }
 }
